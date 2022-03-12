14c439287265b1a68549be95ad9912690cfbad92
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-03-12 16:18:34 +0700

    menambahkan class PosisiKarakterGame

---
+++ PosisiKarakterGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul3_1302200040
+{
+    class PosisiKarakterGame
+    {
+        public PosisiKarakterGame()
+        {
+            Console.Write("berdiri");
+        }
+        public void TombolS()
+        {
+            Console.Write("tombol arah bawah ditekan");
+        }
+        public void TombolW()
+        {
+            Console.Write("tombol arah atas ditekan");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,24 @@ namespace modul3_1302200040
             string buah = Console.ReadLine();
             KodeBuah kode = new KodeBuah();
             Console.WriteLine("Kode Buah : " + kode.getKodeBuah(buah));
+
+            Console.WriteLine("\n-----Posisi Karakter Game-----");
+            Console.Write("State Awal : ");
+            PosisiKarakterGame pos = new PosisiKarakterGame();
+            Console.Write("\nTekan Tombol : ");
+            string state = Console.ReadLine();
+            if (state == "S")
+            {
+                pos.TombolS();
+            }
+            else if (state == "W")
+            {
+                pos.TombolW();
+            }
+            else
+            {
+                Console.WriteLine("Coba tekan tombol S atau W");
+            }
         }
     }
 }
